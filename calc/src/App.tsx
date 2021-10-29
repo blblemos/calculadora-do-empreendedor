dd25dad0f7901e05920040a9b096bb52ecec82f5
blblemos <blblemos17@gmail.com>
2021-10-29 16:17:34 -0300

    Home- html e css

--- calc/src/App.tsx
+++ calc/src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import './styles/index.css';
+
+import logoimg from './img/logo.svg';
+
+import './components/menu/menu.css';
+import './components/slide-home/slide-home.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>OI caraio</h1>
+    <div>
+      <nav>
+        <div className="container-logo-img">
+          <div className="logo-img"></div>
+        </div>
+        <ul>
+          <li>HOME</li>
+          <li>CALCULADORA</li>
+          <li>SOBRE</li>
+        </ul>
+      </nav>
+
+      <main>
+        <div className="text-slide">
+            <h1>A calculadora que você precisa para o seu negócio!</h1>
+        </div>
+        <div className="img-slide">
+        </div>
+      </main>
     </div>
+    
   );
 }
 
